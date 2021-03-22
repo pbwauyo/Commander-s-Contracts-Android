d77b9d778a2b0f2d359e9dfc29fd3d870e45827b
joel <joelcrawford30@gmail.com>
2021-03-22 22:24:01 +0300

    Register Screen

--- app/src/main/java/com/example/commanderscontracts/LoginActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.example.commanderscontracts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         overridePendingTransition(R.anim.go_in, R.anim.go_out)
+
+
+        backToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            //overridePendingTransition(R.anim.slide_out_left, R.anim
+        }
     }
 
 
--- app/src/main/java/com/example/commanderscontracts/RegisterActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/RegisterActivity.kt
@@ -1,14 +1,22 @@
 package com.example.commanderscontracts
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         overridePendingTransition(R.anim.go_in, R.anim.go_out)
+
+        backToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
