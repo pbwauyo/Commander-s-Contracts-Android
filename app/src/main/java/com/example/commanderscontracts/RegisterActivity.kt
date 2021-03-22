fb2f8798eb9ec92e62c0b9cbfaabfe7e16254e82
joel <joelcrawford30@gmail.com>
2021-03-22 14:42:27 +0300

    Animation

--- app/src/main/java/com/example/commanderscontracts/RegisterActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/RegisterActivity.kt
@@ -1,11 +1,29 @@
 package com.example.commanderscontracts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        overridePendingTransition(R.anim.go_in, R.anim.go_out);
+    }
+
+
+    fun back(view: View?) {
+        finishMyActivity()
+    }
+
+
+    fun finishMyActivity(){
+        finish()
+        overridePendingTransition(R.anim.back_in, R.anim.back_out)
+    }
+
+    override fun onBackPressed() {
+
+        finishMyActivity()
     }
 }
