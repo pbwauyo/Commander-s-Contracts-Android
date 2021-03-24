3d1173da9e97c02305a3e39a62a0243eb5193d61
joel <joelcrawford30@gmail.com>
2021-03-24 22:12:24 +0300

    Upload photo

--- app/src/main/java/com/example/commanderscontracts/RegisterActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/RegisterActivity.kt
@@ -1,7 +1,11 @@
 package com.example.commanderscontracts
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
@@ -13,6 +17,9 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    var selectedPhotoUri: Uri? = null  //global variable, because we need to access it
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -22,6 +29,34 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+
+        select_photo_register_button.setOnClickListener {
+
+            Log.d("RegisterActivity", "try to show photo selector")
+
+            //======start photo selector
+
+            //intent for photo selector
+            val intent = Intent(Intent.ACTION_PICK)
+
+            //specifying the type
+            intent.type = ("image/*")
+
+
+            //start activity for result, for the result that comes and request code, we gonna use 0
+            startActivityForResult(intent, 0)
+
+        }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -219,6 +254,38 @@ fun back(view: View?) {
             // cursor reset his position so we need set position to the end of text
             inputConfirmPassword.setSelection(inputConfirmPassword.text.length)
         }
+    }
+
+
+
+
+    //we capture the result from photo selector
+//the method is called when the intent of photo selector is finished
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        //Do a couple of checks
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            //1. we proceed and check what selected image was ---
+            Log.d("RegisterActivity", "photo was selected")
+
+            //2. we have to figure which photo it is inside out app, the pass data has data, uri will represent the location of where the imahe is stored in the device
+
+            val selectedPhotoUri = data.data
+            //3. we can use the uri to get access to the image as a bitmap
+
+            val bitmap =  MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            //4. pace it inside the button
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            select_photo_register_button.setBackgroundDrawable(bitmapDrawable)
+
+
+
+        }
+
+
     }
 
 }
