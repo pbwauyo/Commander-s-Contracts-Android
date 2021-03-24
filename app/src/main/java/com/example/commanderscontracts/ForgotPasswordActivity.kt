984db70bc16df941f121f8579af5d9ab43df4ebf
joel <joelcrawford30@gmail.com>
2021-03-24 13:54:04 +0300

    Show and hide password

--- app/src/main/java/com/example/commanderscontracts/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/ForgotPasswordActivity.kt
@@ -7,5 +7,17 @@ class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+        overridePendingTransition(R.anim.go_in, R.anim.go_out)
+    }
+
+
+    fun finishMyActivity(){
+        finish()
+        overridePendingTransition(R.anim.back_in, R.anim.back_out)
+    }
+
+    override fun onBackPressed() {
+
+        finishMyActivity()
     }
 }
--- app/src/main/java/com/example/commanderscontracts/LoginActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/LoginActivity.kt
@@ -3,6 +3,10 @@ package com.example.commanderscontracts
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_login.*
 
 
@@ -18,8 +22,54 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             //overridePendingTransition(R.anim.slide_out_left, R.anim
         }
+
+
+        forgotPassword.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 
+
+
+//    fun ShowHidePassword(view: View) {
+//
+//        if(view.getId()==R.id.show_pass_btn){
+//            if(inputPasswordLogin.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+//
+//                ((ImageView)(view)).setImageResource(R.drawable.ic_visibility_off_black);
+//
+//                //Show Password
+//
+//            }
+//        }
+//
+//    }
+
+
+    fun ShowHidePass(view:View) {
+        if (view.getId() === R.id.show_pass_btn)
+        {
+            if (inputPasswordLogin.getTransformationMethod().equals(PasswordTransformationMethod.getInstance()))
+            {
+                ((view) as ImageView).setImageResource(R.drawable.ic_visibility_off_black)
+                //Show Password
+                inputPasswordLogin.setTransformationMethod(HideReturnsTransformationMethod.getInstance())
+            }
+            else
+            {
+                ((view) as ImageView).setImageResource(R.drawable.ic_visibility_black)
+                //Hide Password
+                inputPasswordLogin.setTransformationMethod(PasswordTransformationMethod.getInstance())
+            }
+
+            // cursor reset his position so we need set position to the end of text
+            inputPasswordLogin.setSelection(inputPasswordLogin.getText().length)
+        }
+    }
 
 
     fun finishMyActivity(){
