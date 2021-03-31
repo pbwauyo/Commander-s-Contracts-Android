8c2538714f202ddf19cfbb6884f4800c6ff862cc
joel <joelcrawford30@gmail.com>
2021-03-31 16:26:33 +0300

    enum to manage states

--- app/src/main/java/com/example/commanderscontracts/CaptureSignaturesActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/CaptureSignaturesActivity.kt
@@ -7,25 +7,53 @@ import kotlinx.android.synthetic.main.activity_capture_signatures.*
 import kotlinx.android.synthetic.main.activity_signature.*
 
 class CaptureSignaturesActivity : AppCompatActivity(),OnSignedCaptureListener {
+    var isUserOrContractor: Int? = null
+
+    enum class WhichButton {
+        CLIENT_SIGN,
+        CONTRACTOR_SIGN,
+        DEFAULT
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capture_signatures)
+
+        isUserOrContractor = WhichButton.DEFAULT.ordinal
 
         //====Set title
         supportActionBar?.title = "CAPTURE SIGNATURES"
 
 
-            contractor_signature_button.setOnClickListener { showDialog() }
+            contractor_signature_button.setOnClickListener {
+                isUserOrContractor = WhichButton.CONTRACTOR_SIGN.ordinal
+                showDialog()
+            }
 
-            client_signature_button.setOnClickListener { showDialog() }
+            client_signature_button.setOnClickListener {
+                showDialog()
+
+                isUserOrContractor = WhichButton.CLIENT_SIGN.ordinal
+            }
 
 
     }
 
     override fun onSignatureCaptured(bitmap: Bitmap, fileUri: String) {
 
-        client_image_view.setImageBitmap(bitmap)
-        contractor_image_view.setImageBitmap(bitmap)
+        if(isUserOrContractor ==  WhichButton.CLIENT_SIGN.ordinal ){
+
+            client_image_view.setImageBitmap(bitmap)
+
+        } else {
+
+            contractor_image_view.setImageBitmap(bitmap)
+
+        }
+
+
+
+
 
     }
 
