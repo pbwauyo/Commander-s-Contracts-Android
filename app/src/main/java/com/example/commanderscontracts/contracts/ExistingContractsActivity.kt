bc52a43088295a6e7cf490b933eef0f82e68ff66
joel <joelcrawford30@gmail.com>
2021-04-04 17:42:10 +0300

    Sort data by name

--- app/src/main/java/com/example/commanderscontracts/contracts/ExistingContractsActivity.kt
+++ app/src/main/java/com/example/commanderscontracts/contracts/ExistingContractsActivity.kt
@@ -1,8 +1,8 @@
 package com.example.commanderscontracts.contracts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.commanderscontracts.R
 import com.example.commanderscontracts.models.UserContract
@@ -15,6 +15,9 @@ import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_existing_contracts.*
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
 class ExistingContractsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,27 +49,41 @@ class ExistingContractsActivity : AppCompatActivity() {
     private fun fetchUserContracts() {
 
         val currentUserId  = FirebaseAuth.getInstance().uid ?: return
-        val reference = FirebaseDatabase.getInstance().getReference("/user-signatures/$currentUserId")
+        val reference = FirebaseDatabase.getInstance().getReference("/user-signatures/$currentUserId").orderByChild("clientName")
 
-        reference.addListenerForSingleValueEvent(object: ValueEventListener{
+        reference.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
-                val adapter =  GroupAdapter<GroupieViewHolder>()
+                var adapter = GroupAdapter<GroupieViewHolder>()
 
                 //====will get called every time we call a user
-                snapshot.children.forEach{
-                    Log.d("ExistingContract", it.toString() )
+                snapshot.children.forEach {
+                    Log.d("ExistingContract", it.toString())
 
                     //====convert the value to UserContract object====
 
                     val userContract = it.getValue(UserContract::class.java)
 
+//                    val sortedList: List<UserContract> = ArrayList()
+//                    if (sortedList.size() > 0) {
+//                        Collections.sort(list, Comparator<Any?> { object1, object2 -> object1.getProfession().compareTo(object2.getProfession()) })
+//                    }
 
-                    if(userContract != null) {
+
+
+
+
+                    if (userContract != null) {
 
                         adapter.add(ContractListRow(userContract))
 
                     }
+
+
+//                    adapter.sortedBy{
+//                        it.clientName
+//
+//                    }
 
 
                     recycler_view_existing_contracts.adapter = adapter
@@ -74,9 +91,7 @@ class ExistingContractsActivity : AppCompatActivity() {
                     recycler_view_existing_contracts.addItemDecoration(DividerItemDecoration(this@ExistingContractsActivity, DividerItemDecoration.VERTICAL))
 
 
-
                 }
-
 
 
             }
